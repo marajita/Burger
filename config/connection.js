2d60c70a7a91c8944f871e5edf07958338608c49
marajita <rajita.maharjan@gmail.com>
2019-02-04 22:19:06 -0500

    connection fixed

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
-
+var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  var connection = mysql.createConnection({
     host: "localhost",
+    port: 8889,
     user: "root",
     password: "root",
     database: "burgers_db"
